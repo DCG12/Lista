38d261391e7804f40951906ff25a37730c259db7
David <46406163y@iespoblenou.org>
2016-10-28 16:20:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a46406163y/lista/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.example.a46406163y.lista;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+                        ListView lvCards = (ListView) view.findViewById(R.id.lvCards);
+
+                        return view;
+    }
+}
